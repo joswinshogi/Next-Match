bd4f985bcc9ee97e3a4a46ea63bf8641483ba4d4
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2024-08-27 23:02:13 +0530

    feat: Add member photos page and components

---
+++ NextJS/code/src/app/members/[userId]/chat/page.tsx
@@ -0,0 +1,16 @@
+import { CardBody, CardHeader, Divider } from "@nextui-org/react";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">
+        Chats
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        Chat go here
+      </CardBody>
+    </>
+  );
+}
